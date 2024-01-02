d294dc3a9d87adb5a6a842daa7cb8a93c5d58427
doge-hue <dogecheems858@gmail.com>
2024-01-02 20:37:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
+import Default from "./components/Default";
 
 function App() {
+  const pages = [
+    {
+      Data: {
+        Description: "Home Description.",
+        title: "Home Title",
+        keyword: "Keyword",
+      },
+      PageName: "Home",
+    },
+    {
+      Data: {
+        Description: "About Description",
+        title: "About Title",
+        keyword: "Keyword",
+      },
+      PageName: "About",
+    },
+    {
+      Data: {
+        Description: "Service Description.",
+        title: "Service Title",
+        keyword: "Keyword",
+      },
+      PageName: "Service",
+    },
+    {
+      Data: {
+        Description: "Contact Description",
+        title: "Contact title",
+        keyword: "Keyword",
+      },
+      PageName: "Contact",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Default />}></Route>
+        <Route
+          exact
+          path="/home"
+          element={<Home pageData={pages[0]} />}
+        ></Route>
+        <Route
+          exact
+          path="/about"
+          element={<About pageData={pages[1]} />}
+        ></Route>
+        <Route
+          exact
+          path="/service"
+          element={<Service pageData={pages[2]} />}
+        ></Route>
+        <Route
+          exact
+          path="/contact"
+          element={<Contact pageData={pages[3]} />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Service.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+
+const Service = ({pageData}) => {
+  const {PageName, Data} = pageData;
+  return (
+    <div>
+      <Helmet>
+        <title>{Data.title}</title>
+        <meta name="description" content={Data.Description} />
+      </Helmet>
+      <h1>{PageName}</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/service">Service</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Service
